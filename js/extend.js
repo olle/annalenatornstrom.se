2048f7405c9aa16ef37ffa7713d7c7c8784b2aea
Olle Törnström <olle@studiomediatech.com>
2015-03-17 15:00:32 +0100

    Add extended column layout
    * Now supports extended columns of 2-3 and 4 depending on the
      viewport width.

--- js/extend.js
+++ js/extend.js
@@ -1,16 +1,22 @@
-(function ($win, $doc, $$) {
+(function($win, $doc, $$) {
 
-  var hires = function () {
+  var hires = function() {
     var images = $doc.body.querySelectorAll('.alt-image img');
-    Array.prototype.slice.call(images).forEach(function ($img) {
-      $img.setAttribute('src', $img.getAttribute('src').replace('@lowres', '@orig'));
+    Array.prototype.slice.call(images).forEach(function($img) {
+      $img.setAttribute('src', $img.getAttribute('src').replace(
+        '@lowres',
+        '@orig'));
     });
   };
 
-  $$.onImageAppended = function () {
+  $$.onImageAppended = function() {
     hires();
   };
 
   hires();
+
+  if ($win.screen.width > 600) {
+    $$.appendImages(2);
+  }
 
 })(window, document, window['_alt_']);
